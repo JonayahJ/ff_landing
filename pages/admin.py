6e7967e254806912e53f922e28694765031bd906
Jonayah Jackson <jonayah@thinkhalcyon.com>
2021-06-10 20:39:11 -0400

    added model admin for the expert

--- pages/admin.py
+++ pages/admin.py
@@ -1,7 +1,19 @@
 from pages.models import Expert
 from django.contrib import admin
 from .models import Expert
+from django.utils.html import format_html
 
 # Register your models here.
 
-admin.site.register(Expert)
+class ExpertAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="40" style="border-radius: 50px" />'.format(object.headshot.url))
+    
+    thumbnail.short_description = "Headshot"
+    
+    list_display = ("id", "thumbnail", "name", "designation", "created_at")
+    list_display_links = ("id", "thumbnail", "name")
+    search_fields = ("name", "designation")
+    list_filter = ("designation",)
+
+admin.site.register(Expert, ExpertAdmin)
